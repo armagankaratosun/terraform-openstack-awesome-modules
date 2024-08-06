949079bd4b8354e127a5fbe2bf1efd2c5505f007
Armagan Karatosun <armagankaratosun@gmail.com>
2024-08-06 13:05:21 +0200

    Update output.tf

--- modules/instances/output.tf
+++ modules/instances/output.tf
@@ -1,4 +1,11 @@
-output "instance_id" {
-  description = "IDs of the created VM instances"
-  value       = openstack_compute_instance_v2.vm.*.id
+output "vm_names" {
+  value = openstack_compute_instance_v2.vm.*.name
+}
+
+output "vm_ip_addresses" {
+  value = openstack_compute_instance_v2.vm.*.access_ip_v4
+}
+
+output "vm_instance_id" {
+  value = openstack_compute_instance_v2.vm.*.id
 }
