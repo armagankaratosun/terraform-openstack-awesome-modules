75f853621410f94c4201902be20447e9a8fc10f3
Armagan Karatosun <armagankaratosun@gmail.com>
2024-08-06 13:05:04 +0200

    moving outputs out from main.tf

--- modules/instances/main.tf
+++ modules/instances/main.tf
@@ -24,16 +24,4 @@ resource "openstack_compute_instance_v2" "vm" {
   lifecycle {
     ignore_changes = [image_id]
   }
-
-output "vm_names" {
-  value = openstack_compute_instance_v2.vm.*.name
-}
-
-output "vm_ip_addresses" {
-  value = openstack_compute_instance_v2.vm.*.access_ip_v4
-}
-
-output "vm_instance_id" {
-  value = openstack_compute_instance_v2.vm.*.id
-}
 }
