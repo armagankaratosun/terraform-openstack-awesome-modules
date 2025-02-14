8e6ffaacca118a5b00ddf4b359fea70c6814ee16
Armagan Karatosun <armagankaratosun@gmail.com>
2025-02-14 12:03:56 +0100

    now using port_id instead of the vm_id

--- modules/associate-fip/variables.tf
+++ modules/associate-fip/variables.tf
@@ -2,7 +2,7 @@ variable openstack_vm_floatingip {
    type = list
    description = "fixed public ip address for the vm, can be a list of ips"
 }
-variable "vm_instance_id" {
+variable "vm_port_id" {
     type = list
-    description = "instance ids of the vms that will be assigned with a floating ip"
+    description = "port ids of the vms that will be assigned with a floating ip"
 }
